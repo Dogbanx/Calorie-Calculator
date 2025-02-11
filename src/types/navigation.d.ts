2f4114ac39c486670a8697d9d5afd821f254723e
Dogbanx <dogbanx@gmail.com>
2025-02-11 09:52:42 +0200

    typo

--- src/types/navigation.d.ts
+++ src/types/navigation.d.ts
@@ -3,28 +3,24 @@ import { NavigatorScreenParams } from '@react-navigation/native'
 import { CompositeScreenProps } from '@react-navigation/native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-// Основний стек додатку
 export type ApplicationStackParamList = {
   Main: NavigatorScreenParams<MainTabParamList>
   notifications: undefined
   settings: undefined
 }
 
-// Таби внизу екрану
 export type MainTabParamList = {
   home: undefined
   profile: undefined
   statistic: undefined
 }
 
-// Тип для екранів вкладеного табу
 export type MainTabScreenProps<T extends keyof MainTabParamList> =
   CompositeScreenProps<
     BottomTabScreenProps<MainTabParamList, T>,
     ApplicationStackScreenProps<keyof ApplicationStackParamList>
   >
 
-// Тип для основного стеку додатку
 export type ApplicationStackScreenProps<
   T extends keyof ApplicationStackParamList,
 > = NativeStackScreenProps<ApplicationStackParamList, T>
